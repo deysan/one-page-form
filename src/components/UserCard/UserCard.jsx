891bec2e70f18a89b5583d139a2e48535f1e0cb1
deysan <ideysan@gmail.com>
2022-07-18 19:48:59 +0300

    Update user card

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,14 +1,26 @@
 import Placeholder from './avatar-placeholder.svg';
 import styles from './UserCard.module.scss';
 
-export const UserCard = () => {
+export const UserCard = ({ name, position, email, phone }) => {
   return (
     <div className={styles.userCard}>
       <div className={styles.userImage}>
         <img src={Placeholder} alt="Photo" width="70" height="70" aria-hidden />
       </div>
-      <div className={styles.userName}></div>
-      <div className={styles.userInfo}></div>
+      <p className={styles.userName}>{name}</p>
+      <div className={styles.userInfo}>
+        <span>{position}</span>
+        <br />
+        <a className={styles.tooltip} href={`mailto:${email}`}>
+          {email}
+          <span className={styles.tooltiptext}>{email}</span>
+        </a>
+        <br />
+        <a className={styles.tooltip} href={`tel:${phone}`}>
+          {phone}
+          <span className={styles.tooltiptext}>{phone}</span>
+        </a>
+      </div>
     </div>
   );
 };
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,20 +2,74 @@ import { Button, Container, UserCard } from '..';
 
 import styles from './Users.module.scss';
 
+const users = [
+  {
+    id: '30',
+    name: 'Angel',
+    email: 'angel.williams@example.com',
+    phone: '+380496540023',
+    position: 'Designer',
+    position_id: '4',
+    registration_timestamp: 1537777441,
+    photo:
+      'https://frontend-test-assignment-api.abz.agency/images/users/5b977ba13fb3330.jpeg'
+  },
+  {
+    id: '29',
+    name: 'Mattie',
+    email: 'mattie.lee@example.com',
+    phone: '+380204819073',
+    position: 'Designer',
+    position_id: '4',
+    registration_timestamp: 1537691099,
+    photo:
+      'https://frontend-test-assignment-api.abz.agency/images/users/5b977ba1245cc29.jpeg'
+  },
+  {
+    id: '36',
+    name: 'Joshua',
+    email: 'joshua.dean@example.com',
+    phone: '+380542161925',
+    position: 'Designer',
+    position_id: '4',
+    registration_timestamp: 1537661281,
+    photo:
+      'https://frontend-test-assignment-api.abz.agency/images/users/5b977ba1e527036.jpeg'
+  },
+  {
+    id: '37',
+    name: 'Lisa',
+    email: 'lisa.medina@example.com',
+    phone: '+380564753087',
+    position: 'Security',
+    position_id: '3',
+    registration_timestamp: 1537639019,
+    photo:
+      'https://frontend-test-assignment-api.abz.agency/images/users/5b977ba20bd9537.jpeg'
+  },
+  {
+    id: '42',
+    name: 'Lorraine',
+    email: 'lorraine.morris@example.com',
+    phone: '+380945198009',
+    position: 'Designer',
+    position_id: '4',
+    registration_timestamp: 1537629182,
+    photo:
+      'https://frontend-test-assignment-api.abz.agency/images/users/5b977ba293d2f42.jpeg'
+  }
+];
+
 export const Users = () => {
   return (
-    <section className={styles.users}>
+    <section className={styles.users} id="users">
       <Container>
         <div className={styles.wrapper}>
           <h1 className={styles.title}>Working with GET request</h1>
           <ul className={styles.list}>
-            <UserCard />
-            <UserCard />
-            <UserCard />
-            <UserCard />
-            <UserCard />
-            <UserCard />
-            <UserCard />
+            {users.map((user) => (
+              <UserCard key={user.id} {...user} />
+            ))}
           </ul>
           <Button title="Show more" />
         </div>
