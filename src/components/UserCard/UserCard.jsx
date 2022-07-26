517d6f8f9186e5333808117928c6c2cc7a7d4b22
deysan <ideysan@gmail.com>
2022-07-26 22:59:17 +0300

    Fix tooltip display

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -3,23 +3,32 @@ import styles from './UserCard.module.scss';
 
 export const UserCard = ({ name, position, email, phone }) => {
   return (
-    <div className={styles.userCard}>
-      <div className={styles.userImage}>
-        <img src={Placeholder} alt="Photo" width="70" height="70" aria-hidden />
-      </div>
-      <p className={styles.userName}>{name}</p>
-      <div className={styles.userInfo}>
-        <span>{position}</span>
-        <br />
-        <a className={styles.tooltip} href={`mailto:${email}`}>
-          {email}
-          <span className={styles.tooltiptext}>{email}</span>
-        </a>
-        <br />
-        <a className={styles.tooltip} href={`tel:${phone}`}>
-          {phone}
-          <span className={styles.tooltiptext}>{phone}</span>
-        </a>
+    <div className={styles.userWrapper}>
+      <div className={styles.userCard}>
+        <div className={styles.userImage}>
+          <img
+            src={Placeholder}
+            alt="Photo"
+            width="70"
+            height="70"
+            aria-hidden
+          />
+        </div>
+        <p className={styles.userName}>{name}</p>
+        <div className={styles.userInfo}>
+          <span>{position}</span>
+          <br />
+          <a className={styles.text} href={`mailto:${email}`}>
+            {email}
+            <span className={styles.tooltipWrapper}>
+              <span className={styles.tooltipText}>{email}</span>
+            </span>
+          </a>
+          <br />
+          <a className={styles.text} href={`tel:${phone}`}>
+            {phone}
+          </a>
+        </div>
       </div>
     </div>
   );
