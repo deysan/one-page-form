007275640f19ff632799071b13b4adb1294fcaae
deysan <ideysan@gmail.com>
2022-07-27 20:03:39 +0300

    Add validate form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,9 +1,11 @@
 import { Button, Container } from '..';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import classnames from 'classnames';
 import { client } from '../../config';
 import styles from './Form.module.scss';
+import { validator } from '../../utils';
+import { validatorConfig } from './validator-config';
 
 export const Form = () => {
   const [token, setToken] = useState('');
@@ -12,23 +14,38 @@ export const Form = () => {
     name: '',
     email: '',
     phone: '',
-    position_id: 1,
+    position_id: '1',
     file: ''
   });
+  const [dirtyInput, setDirtyInput] = useState({
+    name: false,
+    email: false,
+    phone: false
+  });
+  const [errors, setErrors] = useState({});
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-
     setFormData((prevState) => ({
       ...prevState,
-      [name]: name === 'position' ? Number(value) : value
+      [name]: value
+    }));
+  };
+
+  const handleBlur = (event) => {
+    const { name } = event.target;
+    setDirtyInput((prevState) => ({
+      ...prevState,
+      [name]: true
     }));
   };
 
   const getFormSubmit = () => {
-    client
-      .post('users', formData, { headers: { Token: token } })
-      .then((response) => console.log(response));
+    // const errors = validator(formData, validatorConfig);
+    // setErrors(errors);
+    // client
+    //   .post('users', formData, { headers: { Token: token } })
+    //   .then((response) => console.log(response));
   };
 
   const getToken = () => {
@@ -40,6 +57,17 @@ export const Form = () => {
       .get('positions')
       .then((response) => setPositions(response.positions));
   };
+
+  const validate = useCallback((data) => {
+    const errors = validator(data, validatorConfig);
+    setErrors(errors);
+
+    return Object.keys(errors).length === 0;
+  }, []);
+
+  useEffect(() => {
+    validate(formData);
+  }, [formData, validate]);
 
   useEffect(() => {
     getToken();
@@ -61,8 +89,8 @@ export const Form = () => {
               <div className={styles.contacts}>
                 <div
                   className={classnames(
-                    styles.contactField
-                    // styles.contactFieldError
+                    styles.contactField,
+                    errors.name && dirtyInput.name && styles.contactFieldError
                   )}
                 >
                   <input
@@ -70,43 +98,66 @@ export const Form = () => {
                     name="name"
                     id="name"
                     value={formData.name}
-                    required
+                    onBlur={handleBlur}
                     onChange={handleChange}
+                    className={classnames(
+                      formData.name.length > 0 &&
+                        dirtyInput.name &&
+                        styles.dirty
+                    )}
                   />
                   <label htmlFor="name">Your name</label>
-                  {/* <span className={styles.helper}>Helper text</span> */}
+                  {errors.name && dirtyInput.name && (
+                    <span className={styles.helper}>{errors.name}</span>
+                  )}
                 </div>
                 <div
                   className={classnames(
-                    styles.contactField
-                    // styles.contactFieldError
+                    styles.contactField,
+                    errors.email && dirtyInput.email && styles.contactFieldError
                   )}
                 >
                   <input
                     type="email"
                     name="email"
                     id="email"
-                    required
+                    onBlur={handleBlur}
                     onChange={handleChange}
+                    className={classnames(
+                      formData.email.length > 0 &&
+                        dirtyInput.email &&
+                        styles.dirty
+                    )}
                   />
                   <label htmlFor="email">Email</label>
-                  {/* <span className={styles.helper}>Helper text</span> */}
+                  {errors.email && dirtyInput.email && (
+                    <span className={styles.helper}>{errors.email}</span>
+                  )}
                 </div>
                 <div
                   className={classnames(
-                    styles.contactField
-                    // styles.contactFieldError
+                    styles.contactField,
+                    errors.phone && dirtyInput.phone && styles.contactFieldError
                   )}
                 >
                   <input
                     type="tel"
                     name="phone"
                     id="phone"
-                    required
+                    onBlur={handleBlur}
                     onChange={handleChange}
+                    className={classnames(
+                      formData.phone.length > 0 &&
+                        dirtyInput.phone &&
+                        styles.dirty
+                    )}
                   />
                   <label htmlFor="phone">Phone</label>
-                  <span className={styles.helper}>+38 (XXX) XXX - XX - XX</span>
+                  {errors.phone && (
+                    <span className={styles.helper}>
+                      +38 (XXX) XXX - XX - XX
+                    </span>
+                  )}
                 </div>
               </div>
             </fieldset>
@@ -120,8 +171,9 @@ export const Form = () => {
                       name="position"
                       value={position.id}
                       id={position.id}
-                      defaultChecked={formData.position_id === position.id}
-                      // checked={formData.position === position.id}
+                      defaultChecked={
+                        formData.position_id === position.id.toString()
+                      }
                       onChange={handleChange}
                     />
                     <label htmlFor={position.id}>{position.name}</label>
@@ -142,7 +194,13 @@ export const Form = () => {
                 Upload your photo
               </legend>
               <label htmlFor="photo">Upload your photo</label>
-              <input type="file" name="photo" id="photo" accept="image/*" />
+              <input
+                type="file"
+                name="photo"
+                id="photo"
+                accept="image/*"
+                // onChange={handleChange}
+              />
               {/* <span className={styles.helper}>Helper text</span> */}
             </fieldset>
             <Button title="Sign up" onClick={() => getFormSubmit()} />
---
+++ src/components/Form/validator-config.js
@@ -0,0 +1,49 @@
+export const validatorConfig = {
+  name: {
+    isRequired: {
+      message: 'Name is required'
+    },
+    minLength: {
+      message: 'Name must contain at least 2 characters',
+      value: 2
+    },
+    maxLength: {
+      message: 'Name must contain no more than 60 characters',
+      value: 60
+    }
+  },
+  email: {
+    isRequired: {
+      message: 'Email is required'
+    },
+    minLength: {
+      message: 'Email must be at least 2 characters',
+      value: 2
+    },
+    maxLength: {
+      message: 'Email must be no more than 100 characters',
+      value: 100
+    },
+    isEmail: {
+      message: 'Email entered incorrectly'
+    }
+  },
+  phone: {
+    isRequired: {
+      message: 'Phone is required'
+    },
+    isPhone: {
+      message: 'Phone entered incorrectly'
+    }
+  },
+  position_id: {
+    isRequired: {
+      message: 'Position is required'
+    }
+  },
+  file: {
+    isRequired: {
+      message: 'File is required'
+    }
+  }
+};
